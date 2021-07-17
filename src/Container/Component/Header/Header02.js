86769e1f0e9ac96ba387811f233a25475deaab41
unknown <harshavarsh1223@gmail.com>
2021-07-18 02:10:09 +0530

    nav

--- src/Container/Component/Header/Header02.js
+++ src/Container/Component/Header/Header02.js
@@ -3,29 +3,53 @@ import { MdLocalPhone, MdEmail } from "react-icons/md";
 import { Link } from 'react-router-dom'
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-
+import { IoIosArrowForward, IoIosHeart } from "react-icons/io";
 class Header02 extends Component {
     render() {
         return (
-            <div className="Header_two">
+           <div className="Header_two">
             <div className="logo"></div>
             <div className="navbar">
                 <ul>
-                    <Link  to="/"><li>Home</li></Link>
-                    <Link to="/"><li>
-                        <div className="drop">
-                            <button className="dropbtn1">Dropdown</button>
+                    <Link to="/"><li className="navli">
+                    <div class="dropdown" >
+                        <button style= {{fontWeight:'bold'}}class="btn  dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                             Home
+                        </button>
+                        <ul class="dropdown-menu" s aria-labelledby="dropdownMenuButton1">
+                            <li><Link style={{ background: 'transparent', color: '#1D1D1D', textAlign: 'center' }} class="dropdown-item" to="#">Washing Machine &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<IoIosArrowForward className='ForwardArrow' /></Link>
+                                <ul class="dropdown-menu dropdown-submenu">
+                                    <li>
+                                        <Link style={{ background: 'transparent', color: '#1D1D1D', textAlign: 'center' }} class="dropdown-item" to="#">Heading 1</Link>
+                                    </li>
+                                    <li>
+                                        <Link style={{ background: 'transparent', color: '#1D1D1D', textAlign: 'center' }} class="dropdown-item" to="#">Heading 1</Link>
+                                    </li>
+                                    <li>
+                                        <Link style={{ background: 'transparent', color: '#1D1D1D', textAlign: 'center' }} class="dropdown-item" to="#">Heading 1</Link>
+                                    </li>
+                                    <li>
+                                        <Link style={{ background: 'transparent', color: '#1D1D1D', textAlign: 'center' }} class="dropdown-item" to="#">Heading 1</Link>
+                                    </li>
+                                    <li></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                        {/* <div className="drop">
+                            <button className="dropbtn1">Home</button>
                             <div className="dropdown-content1">
                                 <p>Washing Machine</p>
                                 <p>Link 2</p>
                                 <p>Link 3</p>
                             </div>
-                        </div>
+                        </div> */}
                     </li></Link>
-                    <Link to='/aboutUs'><li>About us</li></Link>
-                    <Link to='/sendquery'><li>Send query</li></Link>
-                    <Link to='/profile'><li>Complaint</li></Link>
-                    <Link to="/blogs"><li>Blogs</li></Link>
+                    <Link to='/aboutUs'><li className="navli">About us</li></Link>
+                    <Link to='/sendquery'><li className="navli">Send query</li></Link>
+                    <Link to='/profile'><li className="navli">Complaint</li></Link>
+                    <Link to="/blogs"><li className="navli">Blogs</li></Link>
                 </ul>
             </div>
             <div className="Header_two_part3">
